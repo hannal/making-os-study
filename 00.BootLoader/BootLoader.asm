1b3460d5dfaf1cf9563f65f6eb9b8b3aa9cdcf6b
hannal <kay@hannal.net>
2020-06-25 23:19:03 +0900

    Fix 문자열 출력.

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -21,6 +21,28 @@ mov si, 0   ; SI 레지스터(문자열 원본 인덱스 레지스터) 초기화
     cmp si, 80 * 25 * 2 ; 화면의 전체 크기는 80*25
     jl  .SCREENCLEARLOOP    ; SI 레지스터가 80*25*2 보다 작다면 아직 지우지 못한 영역이 있으므로 .SCREENCLEARLOOP 레이블로 이동
 
+    mov si, 0   ; SI 레지스터(문자열 원본 인덱스 레지스터) 초기화
+    mov di, 0   ; DI 레지스터(문자열 대상 인덱스 레지스터) 초기화
+
+.MESSAGELOOP:
+    mov cl, byte [ si + MESSAGE1 ]  ; MESSAGE1의 어드레스에서 si 레지스터 값만큼 더한 위치의 문자를 CL 레지스터에 복사
+                                    ; CL레지스터는 CX레지스터의 하위 1바이트를 의미
+                                    ; 문자열은 1바이트면 충분하므로 CX 레지스터의 하위 1바이트만 사용
+
+    cmp cl, 0   ; 복사된 문자와 0 비교
+    je .MESSAGEEND  ; 복사한 문자의 값이 0이면 문자열이 종료되었음을 의미하므로 .MESSAGEEND로 이동하여 문자 출력 종료
+
+    mov byte [ es: di ], cl ; 0이 아니면 비디오 메모리 어드레스 0xB800:di에 문자를 출력
+
+    add si, 1   ; si 레지스터에 1을 더하여 다음 문자열로 이동
+    add di, 2   ; di 레지스터에 2를 더하여 비디오 메모리의 다음 문자 위치로 이동.
+                ; 비디오 메모리는 (문자, 속성)의 쌍으로 구성되므로 문자만 출력하려면 2를 더해야 함.
+
+    jmp .MESSAGELOOP    ; 메시지 출력 루프로 이동하여 다음 문자를 출력
+.MESSAGEEND:
+
+MESSAGE1:   db 'Mint64 os boot loader start', 0 ; 출력할 메시지 정의. 마지막은 0으로 설정하여 .MESSAGELOOP에서 처리할 수 있게 함.
+
 mov byte [ es: 0x00 ], 'M'  ; DS 세그먼트:오프셋 0xB800:0x0000에 'M' 복사
 mov byte [ es: 0x01 ], 0x4A ; DS 세그먼트:오프셋 0xB800:0x0001에 0x4A(빨간 배경에 밝은 녹색 속성)를 복사
 
